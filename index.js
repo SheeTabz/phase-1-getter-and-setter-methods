4ba9a438cc31821668991fe5fb78bf957c23da3b
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-10 09:38:10 +0300

    Inheritance of props btwn parent and child

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 // Add your Circle class here
 
-// class Bird {
-//     #phrase
-//     constructor(name){
-//         this.name = name;
-//     }
-//     set phrase(phrase){
-//         this.#phrase = phrase;
-//     }
-//     get speak(){
-//         return `${this.name} is a bird that speaks ${this.#phrase}`
-//     }
-// }
-// let bird1 = new Bird("Parrot")
-// console.log(bird1.speak)
-// bird1.phrase = "squaks"
-// console.log(bird1.speak)
-
-
-class Circle{
-
-    constructor(radius){
-        this.radius = radius;
-
+class Bird {
+    #phrase
+    constructor(name){
+        this.name = name;
     }
-    get diameter(){
-        return this.radius * 2
+    set phrase(phrase){
+        this.#phrase = phrase;
     }
-    set diameter(diameter){
-        this.radius = diameter / 2
-    }
-    get circumference(){
-        return this.radius * 2 * Math.PI
-    }
-    set circumference(circumference){
-        this.radius = circumference / (2 * Math.PI)
-    }
-    get area(){
-        return this.radius * this.radius * Math.PI
-    }
-    set area(area){
-        this.radius = Math.sqrt(area/ Math.PI)
+    get speak(){
+        return `${this.name} is a bird that speaks ${this.#phrase}`
     }
 }
+let bird1 = new Bird("Parrot")
+bird1.phrase = "hurrays"
+console.log(bird1.speak)
+bird1.phrase = "squaks"
+console.log(bird1.speak)
+
+
+// class Circle{
+
+//     constructor(radius){
+//         this.radius = radius;
+
+//     }
+//     get diameter(){
+//         return this.radius * 2
+//     }
+//     set diameter(diameter){
+//         this.radius = diameter / 2
+//     }
+//     get circumference(){
+//         return this.radius * 2 * Math.PI
+//     }
+//     set circumference(circumference){
+//         this.radius = circumference / (2 * Math.PI)
+//     }
+//     get area(){
+//         return this.radius * this.radius * Math.PI
+//     }
+//     set area(area){
+//         this.radius = Math.sqrt(area/ Math.PI)
+//     }
+// }
